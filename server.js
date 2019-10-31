8845ea04ef972066fbe975cc1a4bff9305002725
8bitago <eightbitago@gmail.com>
2019-11-01 00:25:16 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const db = require("./config/db");
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, { useUnifiedTopology: true }, (err, database) => {
+  if (err) return console.log(err);
+
+  database = database.db("sample");
+  require("./app/routes")(app, database);
+  app.listen(port, () => {
+    console.log("We are live on " + port);
+  });
+});
